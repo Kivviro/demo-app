3dba1cd045688bfecb2ad3fb9425e6209b8fd131
Kivvi <Leo15062004@gmail.com>
2026-09-24 19:35:58 +0700

    fix all

--- app.py
+++ app.py
@@ -4,11 +4,3 @@ def add(a, b):
 if __name__ == "__main__":
     print(add(2, 3))
 
-test_app.py:
-    from app imprt add
-
-def test_add():
-    assert add(2, 3) == 5
-
-requirements.txt:
-    pytest
